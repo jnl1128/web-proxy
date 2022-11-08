ab2a0203157fab9f82fa036cbd1a77b43b82bcfb
Ubuntu <ubuntu@ip-172-31-10-2.ap-northeast-2.compute.internal>
2022-11-08 03:34:00 +0000

    sequential_proxy (imcomplete valid request check)

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -18,16 +18,20 @@ int main(void) {
 
   /* Make the respones body */
   sprintf(content, "QUERY_STRING=%s", buf);
-  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "<p><img src = "
+                   "../cold_zzang.jpeg"
+                   "></p>\r\n");
+  sprintf(content, "%sWelcome to add.com: \r\n", content);
   sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+  sprintf(content, "%sThe answer is: <b>%d</b> + <b>%d</b> = %d\r\n<p>", content, n1, n2, n1 + n2);
   sprintf(content, "%sThanks for visiting!\r\n", content);
 
   /* Generate the HTTP response */
   printf("Connection: close\r\n");
-  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-Length: %d\r\n", (int)strlen(content));
   printf("Content-type: text/html\r\n\r\n");
-  printf("%s", content);
+  if (strcasecmp(getenv("REQUEST_METHOD"), "HEAD"))
+    printf("%s", content);
   fflush(stdout);
 
   exit(0);
